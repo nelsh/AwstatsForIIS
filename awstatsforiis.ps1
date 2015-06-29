e7db33ac0f0beeb525bf6a5c62c1ca8c50dc3a9e
nelsh <env@virtech.ru>
2015-06-29 15:50:25 +0800

    Initial script and config

---
+++ awstatsforiis.ps1
@@ -0,0 +1,24 @@
+﻿param($task)
+
+function ExitWithMsg($msg) {
+    Write-Host "Error: $msg`n" -foregroundcolor "red"
+    $scriptname=$MyInvocation.ScriptName
+    "Usage:`n"
+    "`t$scriptname -task <taskname>`n"
+    "`twhere 'taskname' is one from: setup, checkcfg, build, summary`n" 
+    exit(1)
+}
+
+function Task-Setup {
+    "Run Setup task"
+}
+
+# Main Procedure
+
+if (!$task) {
+    ExitWithMsg("Not set task")
+}
+switch ($task) {
+    "setup" { Task-Setup }
+    default { ExitWithMsg("Task {0} not found" -f $task ) }
+}
