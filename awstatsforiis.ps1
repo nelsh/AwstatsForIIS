cc38ff5201042957ff29899f0d479846f732c56a
nelsh <env@virtech.ru>
2015-06-29 16:49:38 +0800

    add reading ini-file

--- awstatsforiis.ps1
+++ awstatsforiis.ps1
@@ -13,11 +13,19 @@ function Task-Setup {
     "Run Setup task"
 }
 
-# Main Procedure
-
+#
+# MAIN PROCEDURE
+#
 if (!$task) {
     ExitWithMsg("Not set task")
 }
+# Read parameters from ini-file
+$inifile = Join-Path $PSScriptRoot ( $MyInvocation.MyCommand.Name.Replace("ps1", "ini") )
+if (!(Test-Path $inifile)) {
+    Write-Host ("INI-file not found '{0}'." -f $inifile) -foregroundcolor "red"
+    exit(1)
+}
+
 switch ($task) {
     "setup" { Task-Setup }
     default { ExitWithMsg("Task {0} not found" -f $task ) }
