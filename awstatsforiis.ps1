6329eacb9c58df76831b3f74e76cef1fad136245
nelsh <env@virtech.ru>
2015-07-01 15:10:40 +0800

    create common awstats config

--- awstatsforiis.ps1
+++ awstatsforiis.ps1
@@ -9,18 +9,22 @@ function ExitWithMsg($msg) {
     exit(1)
 }
 
-function Task-Setup {
-    "Run Setup task"
+function Task-Setup-IIS {
+    "Run IIS Setup task"
     $perlexewithpar = $ini["PERLEXE"] + " `"%s`" %s"
+    
+    # Register Perl in IIS
     Add-WebConfiguration system.webServer/security/isapiCgiRestriction `
         -Value @{
             path="$perlexewithpar";
             allowed="true";
             description="Perl CGI"}
 
+    # Add virtual application "Awstats" to site SITEFORAWSTATS
     New-Item ('IIS:\Sites\' + $ini["SITEFORAWSTATS"] + '\Awstats') `
          -physicalPath (Join-Path $ini["AWSTATSPATH"] "wwwroot") -type Application
 
+    # Allow execute *.pl scripts in virtual application "Awstats"
     $pspath = ("MACHINE/WEBROOT/APPHOST/" + $ini["SITEFORAWSTATS"] + "/awstats/")
     Add-WebConfigurationProperty -pspath $pspath -Filter system.webServer/handlers -Name . `
         -Value @{
@@ -30,8 +34,40 @@ function Task-Setup {
             modules="CgiModule";
             scriptProcessor="$perlexewithpar";
             resourceType="File";}
+    # Add "awstats.pl" to defaultDocument collection in virtual application "Awstats"
     Add-WebConfigurationProperty -pspath $pspath -Filter system.webServer/defaultDocument/files -Name . `
         -Value @{value="awstats.pl";}
+
+    # Set list logfields
+    Set-WebConfiguration system.applicationHost/sites/siteDefaults/logFile/@logExtFileFlags `
+        -Value $ini["logExtFileFlags"]
+}
+
+function Task-Setup-Awstats {
+    "Run Awstats Setup task"
+    $commonconf="C:\awstats\config\common.conf"
+    # create common config
+    Copy-Item C:\awstats\wwwroot\cgi-bin\awstats.model.conf $commonconf -Force
+    
+    # set common parameters
+    $commonconftxt = (Get-Content $commonconf)  -join "`n"
+    $pars = $ini["AWSTATSCHANGEPARS"].split(",")
+    foreach ($p in $pars) {
+        $commonconftxt = [regex]::Replace($commonconftxt, ($p + "=" + ".+"), $p + "=" + $ini[$p]);
+    }
+    Set-Content $commonconf $commonconftxt
+
+    # enable plugins
+    $plugins = $ini["LoadPlugins"].split(",")
+    foreach ($p in $plugins) {
+        Add-Content $commonconf ("LoadPlugin=`"$p`"")
+    }
+
+}
+
+function Task-Setup {
+    Task-Setup-IIS
+    Task-Setup-Awstats
 }
 
 #
